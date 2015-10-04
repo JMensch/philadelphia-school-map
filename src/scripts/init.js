ad4451ca225490d14754e9cb0e8c5c775bf84d68
JMensch <james@jamesmensch.com>
2015-10-04 13:27:21 -0400

    app properly inits from init.js

--- src/scripts/init.js
+++ src/scripts/init.js
@@ -1,7 +1,38 @@
 'use strict';
+/**
+ * Starts the app
+ * @return {bool}
+ */
 function init() {
     let app = app || {};
+    let geojson;
+    // the location of our geojson file of schools
+    let geojsonLoc = 'https://raw.githubusercontent.com/JMensch/philadelphia-school-map/master/dist/data/data.geojson';
+    // the location of our kml layer file
+    let kmlLoc = 'https://raw.githubusercontent.com/JMensch/philadelphia-school-search/master/dist/data/catchments.kml';
+
+    geojson = _getData(geojsonLoc);
+
+    // init the map
     app.Map.render('map');
+
+    // try to add the geojson layer
+    try {
+        if (geojson) {
+            app.Map.addGeojson(geojson);
+        }
+    } catch (e) {
+        console.log(e);
+    }
+
+    //try to add the kml layer
+    try {
+        app.Map.addKmlLayer(kmlLoc);
+    } catch (e) {
+        console.log(e);
+    }
+
+    return true;
 }
 
 function test(Metadata) {
@@ -10,30 +41,23 @@ function test(Metadata) {
     meta.read();
 }
 
-function _getData() {
-    kmlLoc = 'https://raw.githubusercontent.com/JMensch/philadelphia-school-search/master/dist/data/catchments.kml';
-    geojsonLoc = 'https://raw.githubusercontent.com/JMensch/philadelphia-school-map/master/dist/data/data.geojson';
-}
-export { init, test };
-
-
-// this.kmlLoc =
-
-
-app.Api.get(src)
-    .then(
-        function (geojson) {
-            // if geojson exists, add it
-            if (geojson !== undefined) {
-                if (typeof geojson == 'string') {
-                    geojson = JSON.parse(geojson);
-                }
-
+/**
+ * Gets geojson data
+ * @param  {string} src The location to request data from
+ * @return {object}     Response or empty object
+ */
+function _getData(src) {
+    let app = app || {};
+    app.Api.get(src)
+        .then(
+            function (res) {
+                return res;
+            },
+            function (error) {
+                console.log(error);
+                return {};
             }
-            return true;
-        },
-        function (error) {
-            console.log(error);
-            return false;
-        }
-    );
+        );
+}
+
+export { init, test };
