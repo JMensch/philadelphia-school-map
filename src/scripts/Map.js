25ef62926eae61d37f8b6acbc53065d758c881f4
JMensch <james@jamesmensch.com>
2015-10-13 19:20:13 -0400

    leaflets in, focus and zoom working

--- src/scripts/Map.js
+++ src/scripts/Map.js
@@ -115,13 +115,12 @@ export default class Map {
      */
     render(loc) {
         // default the map to Philly
-        this.map = Leaflet.map(loc).setView([39.99, -75.107], 12);
+        this.map = Leaflet.map(loc).setView([39.99, -75.107], 12)
         Leaflet.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-            attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
             maxZoom: 18,
-            id: 'your.mapbox.project.id',
-            accessToken: 'your.mapbox.public.access.token'
-        }).addTo(map);
+            id: 'menschy28.cifpumc636h7ks6kqjj2kk1uy',
+            accessToken: 'pk.eyJ1IjoibWVuc2NoeTI4IiwiYSI6ImNpZnB1bWRiMGFkZjZpdWx4eXZyYnRwejYifQ.wn5cyDtjQct-_iUl3lI3vQ'
+        }).addTo(this.map);
         return this;
     }
 }
